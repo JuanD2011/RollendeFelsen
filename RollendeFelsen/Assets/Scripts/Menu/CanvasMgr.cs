72728704d1d4f7cb9b75332632543dcb140152c5
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-09-03 13:32:34 -0500

    Menu&&CanvasGame

---
+++ RollendeFelsen/Assets/Scripts/Menu/CanvasMgr.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CanvasMgr : MonoBehaviour {
+
+    [SerializeField] GameObject[] canvas;
+    ButtonMgr buttonMgr;
+    byte count;
+
+    private void Start()
+    {
+        buttonMgr = GetComponentInChildren<ButtonMgr>();
+        buttonMgr.OnUnnpause += Unnpaused;
+        canvas[1].SetActive(false);
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Paused();
+            if (canvas[1].activeInHierarchy && count >= 2)
+            {
+                Unnpaused();
+            }
+        }
+    }
+
+    public void Paused() {
+        count++;
+        canvas[0].SetActive(false);
+        Time.timeScale = 0;
+        canvas[1].SetActive(true);
+    }
+
+    public void Unnpaused() {
+        count = 0;
+        canvas[0].SetActive(true);
+        Time.timeScale = 1;
+        canvas[1].SetActive(false);
+    }
+}
