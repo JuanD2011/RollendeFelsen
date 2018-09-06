9c742039bba3b79fbf933188bb12f4836f51039e
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-09-05 20:37:21 -0500

    RockMela

--- RollendeFelsen/Assets/Scripts/Rock.cs
+++ RollendeFelsen/Assets/Scripts/Rock.cs
@@ -1,11 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rock : MonoBehaviour {
     [SerializeField] private float startForce;
+    Rigidbody mRigidbody;
+
+    private void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+        Invoke("Launch", 1f);
+    }
 
     private void Launch() {
+        mRigidbody.AddForce(-Vector3.forward * startForce, ForceMode.Impulse);
+        Invoke("AutoDestroy", 20f);
+    }
 
+    private void AutoDestroy() {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Actor>() != null) {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
